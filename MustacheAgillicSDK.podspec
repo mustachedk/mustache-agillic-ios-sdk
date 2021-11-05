0dd1e69a363299081e4c07ae97dc49d635cd9e9f
Finn Larsen <fl@mustache.dk>
2021-11-05 09:27:41 +0100

    fixes

--- MustacheAgillicSDK.podspec
+++ MustacheAgillicSDK.podspec
@@ -10,7 +10,7 @@ Pod::Spec.new do |s|
 
   s.ios.deployment_target = '11.0'
 
-  s.source_files = 'Sources/*'
+  s.source_files = 'src/MustacheAgillicSDK/*'
 
   s.frameworks = 'UIFoundation'
 
--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
     .target(
         name: "MustacheAgillicSDK",
         dependencies: ["SnowplowTracker"],
-        path: "Sources")
+        path: "src/MustacheAgillicSDK")
   ]
 )
